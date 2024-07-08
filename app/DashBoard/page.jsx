8b18c1bd9c09d42f571513dffe68421554a6a0f0
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-08 08:07:38 +0500

    ok

---
+++ app/DashBoard/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import AddInterview from '../../components/Add_Interview'
+const page = () => {
+  return (
+    <main className=" p-3">
+      <AddInterview />
+    </main>
+  )
+}
+
+export default page
