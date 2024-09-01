0e81fc531722b7a3c1f97c81cc8e588f3de499fd
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-01 18:05:23 +0500

    routing to dashboard after signin

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -1,5 +1,14 @@
-import { SignIn } from '@clerk/nextjs'
+// pages/sign-in.tsx
 
-export default function Page() {
-  return <SignIn path="/sign-in" />
+import { SignIn } from '@clerk/nextjs'
+import { useRouter } from 'next/router'
+
+export default function SignInPage() {
+  const router = useRouter()
+
+  const handleSignInSuccess = () => {
+    router.push('/DashBoard')
+  }
+
+  return <SignIn path="/sign-in" afterSignIn={handleSignInSuccess} />
 }
